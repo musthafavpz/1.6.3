4c274011c2843aaeb2ffcfad46cefc086a6ec400
musthafavpz <65675930+musthafavpz@users.noreply.github.com>
2025-04-15 18:02:15 +0530

    Update tab_screen.dart

--- lib/screens/tab_screen.dart
+++ lib/screens/tab_screen.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, use_key_in_widget_constructors, use_super_parameters, library_private_types_in_public_api
-
 import 'package:academy_lms_app/constants.dart';
 import 'package:academy_lms_app/screens/account.dart';
 import 'package:academy_lms_app/screens/cart.dart';
@@ -7,12 +5,12 @@ import 'package:academy_lms_app/screens/filter_screen.dart';
 import 'package:academy_lms_app/screens/home.dart';
 import 'package:academy_lms_app/screens/login.dart';
 import 'package:academy_lms_app/screens/my_courses.dart';
+import 'package:academy_lms_app/screens/explore.dart'; // Add this import
 import 'package:academy_lms_app/widgets/appbar_one.dart';
 import 'package:flutter/material.dart';
 import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 
 class TabsScreen extends StatefulWidget {
   final int pageIndex;
@@ -23,16 +21,34 @@ class TabsScreen extends StatefulWidget {
   _TabsScreenState createState() => _TabsScreenState();
 }
 
-class _TabsScreenState extends State<TabsScreen> {
+class _TabsScreenState extends State<TabsScreen> with TickerProviderStateMixin {
   int _selectedPageIndex = 0;
   bool isLoggedIn = false;
   bool _isInit = true;
+  late AnimationController _animationController;
+  late Animation<double> _fadeAnimation;
 
   @override
   void initState() {
     super.initState();
     _selectedPageIndex = widget.pageIndex;
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.easeInOut,
+      ),
+    );
     _checkAuthStatus();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 
   Future<void> _checkAuthStatus() async {
@@ -47,44 +63,93 @@ class _TabsScreenState extends State<TabsScreen> {
 
   List<Widget> _pages() {
     return isLoggedIn
-        ? [HomeScreen(), MyCoursesScreen(), CartScreen(), AccountScreen()]
-        : [HomeScreen(), LoginScreen(), LoginScreen(), LoginScreen()];
+        ? [
+            HomeScreen(),
+            ExploreScreen(), // New Explore screen
+            MyCoursesScreen(),
+            CartScreen(),
+            AccountScreen(),
+          ]
+        : [
+            HomeScreen(),
+            ExploreScreen(), // New Explore screen
+            LoginScreen(),
+            LoginScreen(),
+            LoginScreen(),
+          ];
   }
 
   void _selectPage(int index) {
+    _animationController.reset();
     setState(() {
       _selectedPageIndex = index;
     });
+    _animationController.forward();
+  }
+
+  Widget _buildTabIcon(String assetName, bool isActive) {
+    return AnimatedSwitcher(
+      duration: const Duration(milliseconds: 300),
+      transitionBuilder: (Widget child, Animation<double> animation) {
+        return ScaleTransition(scale: animation, child: child);
+      },
+      child: Padding(
+        key: ValueKey<bool>(isActive),
+        padding: const EdgeInsets.all(8.0),
+        child: SvgPicture.asset(
+          assetName,
+          width: 24,
+          height: 24,
+          colorFilter: ColorFilter.mode(
+            isActive ? kDefaultColor : kGreyLightColor,
+            BlendMode.srcIn,
+          ),
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const AppBarOne(logo: 'light_logo.png'),
-     
-      body: 
-      _isInit
+      body: _isInit
           ? Center(child: CircularProgressIndicator())
-          :
-           IndexedStack(
-              index: _selectedPageIndex,
-              children: _pages(),
+          : FadeTransition(
+              opacity: _fadeAnimation,
+              child: IndexedStack(
+                index: _selectedPageIndex,
+                children: _pages(),
+              ),
             ),
-      floatingActionButton: _selectedPageIndex != 2
+      floatingActionButton: _selectedPageIndex != 3
           ? FloatingActionButton(
               onPressed: () {
                 Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const FilterScreen(),
-                    ));
+                  context,
+                  PageRouteBuilder(
+                    pageBuilder: (context, animation, secondaryAnimation) =>
+                        const FilterScreen(),
+                    transitionsBuilder:
+                        (context, animation, secondaryAnimation, child) {
+                      return FadeTransition(
+                        opacity: animation,
+                        child: child,
+                      );
+                    },
+                  ),
+                );
               },
               backgroundColor: kWhiteColor,
+              elevation: 4,
               shape: RoundedRectangleBorder(
-                  side: const BorderSide(width: 1, color: kDefaultColor),
-                  borderRadius: BorderRadius.circular(100)),
+                side: const BorderSide(width: 1, color: kDefaultColor),
+                borderRadius: BorderRadius.circular(100),
+              ),
               child: SvgPicture.asset(
                 'assets/icons/filter.svg',
+                width: 24,
+                height: 24,
                 colorFilter: const ColorFilter.mode(
                   kBlackColor,
                   BlendMode.srcIn,
@@ -95,91 +160,41 @@ class _TabsScreenState extends State<TabsScreen> {
       bottomNavigationBar: ConvexAppBar(
         items: [
           TabItem(
-            icon: Padding(
-              padding: const EdgeInsets.all(2.0),
-              child: SvgPicture.asset(
-                'assets/icons/home.svg',
-                colorFilter:
-                    const ColorFilter.mode(kGreyLightColor, BlendMode.srcIn),
-              ),
-            ),
-            activeIcon: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SvgPicture.asset(
-                'assets/icons/home.svg',
-                colorFilter:
-                    const ColorFilter.mode(kDefaultColor, BlendMode.srcIn),
-              ),
-            ),
+            icon: _buildTabIcon('assets/icons/home.svg', _selectedPageIndex == 0),
+            activeIcon: _buildTabIcon('assets/icons/home.svg', true),
             title: 'Home',
           ),
           TabItem(
-            icon: Padding(
-              padding: const EdgeInsets.all(2.0),
-              child: SvgPicture.asset(
-                'assets/icons/my_courses.svg',
-                colorFilter:
-                    const ColorFilter.mode(kGreyLightColor, BlendMode.srcIn),
-              ),
-            ),
-            activeIcon: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SvgPicture.asset(
-                'assets/icons/my_courses.svg',
-                colorFilter:
-                    const ColorFilter.mode(kDefaultColor, BlendMode.srcIn),
-              ),
-            ),
-            title: 'My Courses',
+            icon: _buildTabIcon('assets/icons/explore.svg', _selectedPageIndex == 1),
+            activeIcon: _buildTabIcon('assets/icons/explore.svg', true),
+            title: 'Explore',
           ),
           TabItem(
-            icon: Padding(
-              padding: const EdgeInsets.all(2.0),
-              child: SvgPicture.asset(
-                'assets/icons/shopping_bag.svg',
-                colorFilter:
-                    const ColorFilter.mode(kGreyLightColor, BlendMode.srcIn),
-              ),
-            ),
-            activeIcon: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SvgPicture.asset(
-                'assets/icons/shopping_bag.svg',
-                colorFilter:
-                    const ColorFilter.mode(kDefaultColor, BlendMode.srcIn),
-              ),
-            ),
-            title: 'My Cart',
+            icon: _buildTabIcon('assets/icons/my_courses.svg', _selectedPageIndex == 2),
+            activeIcon: _buildTabIcon('assets/icons/my_courses.svg', true),
+            title: 'Courses',
           ),
           TabItem(
-            icon: Padding(
-              padding: const EdgeInsets.all(2.0),
-              child: SvgPicture.asset(
-                'assets/icons/account.svg',
-                colorFilter:
-                    const ColorFilter.mode(kGreyLightColor, BlendMode.srcIn),
-              ),
-            ),
-            activeIcon: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: SvgPicture.asset(
-                'assets/icons/account.svg',
-                colorFilter:
-                    const ColorFilter.mode(kDefaultColor, BlendMode.srcIn),
-              ),
-            ),
+            icon: _buildTabIcon('assets/icons/shopping_bag.svg', _selectedPageIndex == 3),
+            activeIcon: _buildTabIcon('assets/icons/shopping_bag.svg', true),
+            title: 'Cart',
+          ),
+          TabItem(
+            icon: _buildTabIcon('assets/icons/account.svg', _selectedPageIndex == 4),
+            activeIcon: _buildTabIcon('assets/icons/account.svg', true),
             title: 'Account',
           ),
         ],
         backgroundColor: kWhiteColor,
         color: kGreyLightColor,
-        activeColor: kBlackColor,
-        elevation: 0,
-        style: TabStyle.react,
+        activeColor: kDefaultColor,
+        elevation: 4,
+        curveSize: 100,
+        style: TabStyle.reactCircle,
         initialActiveIndex: _selectedPageIndex,
         onTap: _selectPage,
+        top: -20,
       ),
     );
   }
 }
-
